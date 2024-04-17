886b32da465887602046f940d40ce88ab96fbe26
MohamedUmar083 <umarmohd777@gmail.com>
2024-04-17 19:30:11 +0530

    callback-hell/Task

---
+++ script.js
@@ -0,0 +1,35 @@
+const hid = document.getElementById("message");
+
+setTimeout(() => {
+  hid.innerHTML = "<b>10</b>";
+  setTimeout(() => {
+    hid.innerHTML = "<b>9</b>";
+    setTimeout(() => {
+      hid.innerHTML = "<b>8</b>";
+      setTimeout(() => {
+        hid.innerHTML = "<b>7</b>";
+        setTimeout(() => {
+          hid.innerHTML = "<b>6</b>";
+          setTimeout(() => {
+            hid.innerHTML = "<b>5</b>";
+            setTimeout(() => {
+              hid.innerHTML = "<b>4</b>";
+              setTimeout(() => {
+                hid.innerHTML = "<b>3</b>";
+                setTimeout(() => {
+                  hid.innerHTML = "<b>2</b>";
+                  setTimeout(() => {
+                    hid.innerHTML = "<b>1</b>";
+                    setTimeout(() => {
+                      hid.innerHTML = "<b>🎆🎇Happy Independence Day🎇🎆</b>";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
